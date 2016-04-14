2f9a60244312d94386a7c53bf251f0623c2fce81
Alek Festekjian <festekj2@illinois.edu>
2016-04-14 13:09:37 -0500

    getting rid of extra alerts

--- source_js/controllers.js
+++ source_js/controllers.js
@@ -69,10 +69,7 @@ mp4Controllers.controller('TaskListController', ['$scope','$http','$window' ,'Ta
     $scope.remove = function(id){
         Tasks.getTask(id).success(function(task_data){
             $scope.ownerID = task_data.data.assignedUser;
-            alert($scope.ownerID);
-            console.log(task_data.data);
             if($scope.ownerID != ""){
-                console.log($scope.ownerID);
                 Users.getUser($scope.ownerID).success(function(response){
                     $scope.user = response.data;
                     // console.log($scope.user.pendingTasks);
